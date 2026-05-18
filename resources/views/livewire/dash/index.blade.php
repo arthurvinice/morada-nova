2e0405d304c3473bc967cd81b0c6ad09e70c2b01
arthurvinice <arthurvinice@gmail.com>
2026-05-18 19:51:08 -0300

    pequenos ajustes

--- resources/views/livewire/dash/index.blade.php
+++ resources/views/livewire/dash/index.blade.php
@@ -1,3 +1,93 @@
 <div>
-     <p>Do your work, then step back. </p>
+     <div class="card">
+          <h5 class="card-header">Próximos Alugueis</h5>
+          <div class="table-responsive text-nowrap">
+               <table class="table">
+                    <thead>
+                         <tr>
+                              <th>Nome</th>
+                              <th>Endereço</th>
+                              <th>Data de Vencimento</th>
+                              <th>Status</th>
+                              <th>Actions</th>
+                         </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                         <tr>
+                              <td>
+                                   <i class="icon-base ti tabler-brand-angular icon-md text-danger me-4"></i>
+                                   <span class="fw-medium">Angular Project</span>
+                              </td>
+                              <td>Albert Cook</td>
+                              <td>03/04/2021</td>
+                              <td><span class="badge bg-label-primary me-1">Active</span></td>
+                              <td>
+                                   <div class="dropdown">
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                             <i class="icon-base ti tabler-dots-vertical"></i>
+                                        </button>
+                                        <div class="dropdown-menu">
+                                             <a class="dropdown-item waves-effect" href="javascript:void(0);"><i class="icon-base ti tabler-pencil me-1"></i> Edit</a>
+                                             <a class="dropdown-item waves-effect" href="javascript:void(0);"><i class="icon-base ti tabler-trash me-1"></i> Delete</a>
+                                        </div>
+                                   </div>
+                              </td>
+                         </tr>
+                         <tr>
+                              <td><i class="icon-base ti tabler-brand-react-native icon-md text-info me-4"></i> <span class="fw-medium">React Project</span></td>
+                              <td>Barry Hunter</td>
+                              <td>03/04/2021</td>
+                              <td><span class="badge bg-label-success me-1">Completed</span></td>
+                              <td>
+                                   <div class="dropdown">
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                             <i class="icon-base ti tabler-dots-vertical"></i>
+                                        </button>
+                                        <div class="dropdown-menu">
+                                             <a class="dropdown-item waves-effect" href="javascript:void(0);"><i class="icon-base ti tabler-pencil me-2"></i> Edit</a>
+                                             <a class="dropdown-item waves-effect" href="javascript:void(0);"><i class="icon-base ti tabler-trash me-2"></i> Delete</a>
+                                        </div>
+                                   </div>
+                              </td>
+                         </tr>
+                         <tr>
+                              <td>
+                                   <i class="icon-base ti tabler-brand-vue icon-md text-success me-4"></i>
+                                   <span class="fw-medium">VueJs Project</span>
+                              </td>
+                              <td>Trevor Baker</td>
+                              <td>03/04/2021</td>
+                              <td><span class="badge bg-label-info me-1">Scheduled</span></td>
+                              <td>
+                                   <div class="dropdown">
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                             <i class="icon-base ti tabler-dots-vertical"></i>
+                                        </button>
+                                        <div class="dropdown-menu">
+                                             <a class="dropdown-item waves-effect" href="javascript:void(0);"><i class="icon-base ti tabler-pencil me-2"></i> Edit</a>
+                                             <a class="dropdown-item waves-effect" href="javascript:void(0);"><i class="icon-base ti tabler-trash me-2"></i> Delete</a>
+                                        </div>
+                                   </div>
+                              </td>
+                         </tr>
+                         <tr>
+                              <td>
+                                   <i class="icon-base ti tabler-brand-bootstrap icon-md text-primary me-4"></i>
+                                   <span class="fw-medium">Bootstrap Project</span>
+                              </td>
+                              <td>Jerry Milton</td>
+                              <td>03/04/2021</td>
+                              <td><span class="badge bg-label-warning me-1">Pending</span></td>
+                              <td>
+                                   <a class="btn rounded-pill btn-outline-secondary waves-effect btn-sm"
+                                        href="javascript:void(0);">
+                                        <i class="icon-base ti tabler-eye "></i>
+                                   </a>
+
+                              </td>
+                         </tr>
+                    </tbody>
+               </table>
+          </div>
+     </div>
 </div>
