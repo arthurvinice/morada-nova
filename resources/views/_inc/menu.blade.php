24c8f045b3155c04ed7ef7701bf3fb3d73877046
arthurvinice <arthurvinice@gmail.com>
2026-05-20 19:44:29 -0300

    update menu

--- resources/views/_inc/menu.blade.php
+++ resources/views/_inc/menu.blade.php
@@ -1,10 +1,25 @@
 <ul class="menu-inner">
-    
-    <li class="menu-item">
-        <a href="{{ route('admin.people.index') }}" class="menu-link">
+
+    <li class="menu-item {{ request()->routeIs('admin.people.*') ? 'active open' : '' }}">
+        <a href="#" class="menu-link">
             <i class="icon-base ti tabler-users-group icon-md me-2"></i>
             <div data-i18n="Inquilinos">Inquilinos</div>
         </a>
+
+        <ul class="menu-sub">
+            <li class="menu-item {{ request()->routeIs('admin.people.index') ? 'active open' : '' }}">
+                <a href="{{ route('admin.people.index') }}" class="menu-link">
+                    <i class="menu-icon icon-base ti tabler-users-group"></i>
+                    <div data-i18n="Meus Inquilinos">Meus Inquilinos</div>
+                </a>
+            </li>
+            <li class="menu-item {{ request()->routeIs('admin.people.create') ? 'active open' : '' }}">
+                <a href="{{ route('admin.people.create') }}" class="menu-link">
+                    <i class="menu-icon icon-base ti tabler-plus"></i>
+                    <div data-i18n="Novo Inquilino">Novo Inquilino</div>
+                </a>
+            </li>
+        </ul>
     </li>
 
     <li class="menu-item">
