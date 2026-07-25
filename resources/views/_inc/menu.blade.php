b92f5ddbc41372e5729b7fbfe6d53f80a93e148c
arthurvinice <arthurvinice@gmail.com>
2026-07-24 23:10:58 -0300

    ajustando menu e icones

--- resources/views/_inc/menu.blade.php
+++ resources/views/_inc/menu.blade.php
@@ -9,7 +9,7 @@
         <ul class="menu-sub">
             <li class="menu-item {{ request()->routeIs('admin.people.index') ? 'active open' : '' }}">
                 <a href="{{ route('admin.people.index') }}" class="menu-link">
-                    <i class="menu-icon icon-base ti tabler-users-group"></i>
+                    <i class="menu-icon icon-base ti tabler-users"></i>
                     <div data-i18n="Meus Inquilinos">Meus Inquilinos</div>
                 </a>
             </li>
@@ -22,11 +22,25 @@
         </ul>
     </li>
 
-    <li class="menu-item">
-        <a href="{{ route('admin.properties.index') }}" class="menu-link">
-            <i class="icon-base ti tabler-building icon-md me-2"></i>
+    <li class="menu-item {{ request()->routeIs('admin.properties.*') ? 'active open' : '' }}">
+        <a href="#" class="menu-link menu-toggle">
+            <i class="icon-base ti tabler-building-community icon-md me-2"></i>
             <div data-i18n="Propriedades">Propriedades</div>
         </a>
+
+        <ul class="menu-sub">
+            <li class="menu-item {{ request()->routeIs('admin.properties.index') ? 'active open' : '' }}">
+                <a href="{{ route('admin.properties.index') }}" class="menu-link">
+                    <i class="menu-icon icon-base ti tabler-building"></i>
+                    <div data-i18n="Minhas Propriedades">Minhas Propriedades</div>
+                </a>
+            </li>
+            <li class="menu-item {{ request()->routeIs('admin.properties.create') ? 'active open' : '' }}">
+                <a href="{{ route('admin.properties.create') }}" class="menu-link">
+                    <i class="menu-icon icon-base ti tabler-plus"></i>
+                    <div data-i18n="Nova Propriedade">Nova Propriedade</div>
+                </a>
+            </li>
     </li>
 
 </ul>
